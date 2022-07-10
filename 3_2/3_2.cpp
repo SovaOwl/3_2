008be61c0bd046f48700e865f5b3aba35d2360d4
SovaOwl <sova27755@gmail.com>
2022-07-10 12:32:48 +0300

    First commit

---
+++ 3_2/3_2.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Counter
+{
+private:
+	int num = -999;
+
+public:
+	int nume = num;
+	string choice, op_ch;
+
+	void option_choise()
+	{
+		cout << "Вы хотите указать начальное значение счётчика? Введите yes или no: ";
+		cin >> op_ch;
+
+		if (op_ch == "yes")
+		{
+			cout << "Введите начальное значение счётчика: ";
+			cin >> nume;
+		}
+		else if (op_ch == "no")
+		{
+			nume = 1;
+		}
+	}
+
+	void conclusion()
+	{
+		do
+		{
+			cout << "Введите команду ('+', '-', '=' или 'x'): ";
+			cin >> choice;
+
+			if (choice == "+")
+			{
+				nume++;
+
+			}
+			else if (choice == "-")
+			{
+				nume--;
+
+			}
+			else if (choice == "=")
+			{
+				cout << nume << endl;
+
+			}
+			else if (choice == "x")
+			{
+				cout << "До свидания";
+
+			}
+		} while (choice != "x");
+	}
+	
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+
+	Counter coun;
+
+	coun.option_choise();
+
+	coun.conclusion();
+}
